31c62aaa282e8049d6b048896cb0b4217abce4b6
Dewindi-Weerathunga <weerathunga2001@gmail.com>
2026-06-23 21:11:12 +0530

    Create CORS Config

---
+++ config/CORSConfig.js
@@ -0,0 +1,16 @@
+// config/CORSConfig.js
+
+const allowedOrigins = ["http://localhost:4200"];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // allow requests with no origin (like mobile apps, curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
