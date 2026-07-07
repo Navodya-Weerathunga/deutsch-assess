300473fa0b6168745dae725e1440efd83a2bcdc7
Dewindi-Weerathunga <weerathunga2001@gmail.com>
2026-07-07 18:51:22 +0530

    Create Logout option in Admin Side Bar

--- src/app/components/admin-nav-bar/admin-nav-bar.component.ts
+++ src/app/components/admin-nav-bar/admin-nav-bar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 import { MatIconModule } from '@angular/material/icon';
+
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-admin-navbar',
@@ -16,5 +18,36 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './admin-nav-bar.component.css'
 })
 export class AdminNavbarComponent {
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  logout(): void {
+
+    if (!confirm("Are you sure you want to logout?")) {
+      return;
+    }
+
+    this.userService.logout().subscribe({
+
+      next: () => {
+
+        this.router.navigate(['/']);
+
+      },
+
+      error: (err) => {
+
+        console.error(err);
+
+        alert("Logout failed.");
+
+      }
+
+    });
+
+  }
 
 }
