3a1cb921ce191afa29176ac5471ed901603e1524
Dewindi-Weerathunga <weerathunga2001@gmail.com>
2026-08-09 12:54:01 +0530

    Create get all assessment angular service

---
+++ src/app/services/assessment.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+export interface AssessmentQuestion {
+
+  questionNo: number;
+
+  question: string;
+
+  englishQuestion: string;
+
+  marks: number;
+
+}
+
+export interface Assessment {
+
+  _id?: string;
+
+  class: any;
+
+  title: string;
+
+  topic: string;
+
+  level: string;
+
+  language: string;
+
+  instructions: string;
+
+  totalMarks: number;
+
+  questions: AssessmentQuestion[];
+
+  generatedBy?: string;
+
+  createdAt?: string;
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AssessmentService {
+
+  private apiUrl = `${environment.apiUrl}/assessments`;
+
+  constructor(private http: HttpClient) {}
+
+  // =====================================
+  // Get All Assessments
+  // =====================================
+
+  getAllAssessments(): Observable<Assessment[]> {
+
+    return this.http.get<Assessment[]>(
+      this.apiUrl,
+      {
+        withCredentials: true
+      }
+    );
+
+  }
+
+}
